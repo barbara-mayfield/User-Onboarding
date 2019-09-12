9edff4cdd795695396ac6e353d59426397e770ea
Barbara Mayfield <bmayfield1991@gmail.com>
2019-09-12 11:01:13 -0500

    added password and tos fields and a submit button to form, password has a placeholder yup validation while I research how to do that properly

--- user-onboard/src/component/Form.js
+++ user-onboard/src/component/Form.js
@@ -18,8 +18,24 @@ const UserForm = ({ errors, touched, status }) => {
             <Field type="text" name="name" placeholder="Name" />
             {touched.name && errors.name && <p className="error">{errors.name}</p>}
 
+            <Field type="text" name="email" placeholder="E-Mail" />
+            {touched.email && errors.name && <p className="error">{errors.email}</p>}
+
+
+            <Field type="password" name="password" placeholder="Password" />
+            {touched.password && errors.password && <p className="error">{errors.password}</p>}
+
+            <label className="terms">
+            <Field type="checkbox" name="tos" className="termcheck" />
+            <span>Terms of Service</span>
+            </label>
+            
+            <button type="submit">Submit</button>
+            
             {users.map(user => (
-                <div>Name: {user.name}</div>
+                <div>Name: {user.name}
+                    e-mail: {user.email}
+                </div>
             ))}
         </Form>
     )
@@ -30,11 +46,17 @@ const UserForm = ({ errors, touched, status }) => {
 export default withFormik({
     mapPropsToValues: (values) => {
         return {
-            name: values.name || ''
+            name: values.name || '',
+            email: values.email || '',
+            password: values.password || '',
+            tos: values.tos || ''
         }
     },
 
     validationSchema: yup.object().shape({
-        name: yup.string().required('Name is required')
+        name: yup.string().required('Name is required'),
+        email: yup.string().required('Email is required'),
+        password: yup.string().required('Password is required'),
+        tos: yup.boolean().oneOf([true], 'You must accept Terms of Service')
     }),
 })(UserForm);
