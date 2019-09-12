74dc344ae137bbf7c82e1164d2d2b4bee4b25243
Barbara Mayfield <bmayfield1991@gmail.com>
2019-09-12 10:19:36 -0500

    created my Formik form in Form.js, implementing yup validation as I go (step 1 & 2)

--- user-onboard/src/component/Form.js
+++ user-onboard/src/component/Form.js
@@ -1,3 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { withFormik, Form, Field } from 'formik';
+import * as yup from 'yup';
+import axios from 'axios';
 
-export default Form;
+const UserForm = ({ errors, touched, status }) => {
+    console.log(status)
+
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        if (status) {
+            setUsers([...users, status])
+        }}, [status])
+
+    return (
+        <Form>
+            <Field type="text" name="name" placeholder="Name" />
+            {touched.name && errors.name && <p className="error">{errors.name}</p>}
+
+            {users.map(user => (
+                <div>Name: {user.name}</div>
+            ))}
+        </Form>
+    )
+}
+
+
+
+export default withFormik({
+    mapPropsToValues: (values) => {
+        return {
+            name: values.name || ''
+        }
+    },
+
+    validationSchema: yup.object().shape({
+        name: yup.string().required('Name is required')
+    }),
+})(UserForm);
